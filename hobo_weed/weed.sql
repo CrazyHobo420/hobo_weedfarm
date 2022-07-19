f641f164583d8c201e09780029406b959180fdc4
Hobo <fubarferment@gmail.com>
2022-07-19 04:16:28 -0500

    Update weed.sql

--- hobo_weed/weed.sql
+++ hobo_weed/weed.sql
@@ -1,2 +1,3 @@
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('weedsmall', 'Weed Plant - Small', 1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tentlarge', 'Weed Plant - Large', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('weedseed', 'Weed Seeds', 50, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('weedbuds', 'Weed Buds', 50, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan', 'Watering Can', 1, 1, 'item_standard', 0);
